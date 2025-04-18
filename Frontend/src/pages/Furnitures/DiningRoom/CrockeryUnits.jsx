fc802bac379a2cb0350ba0d01bbbe3aff5b7a7f1
sharada417 <sharada.kanchana2001@gmail.com>
2025-04-18 12:22:57 +0530

    Update DiningRoom section

---
+++ Frontend/src/pages/Furnitures/DiningRoom/CrockeryUnits.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CrockeryUnits = () => {
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  )
+}
+
+export default CrockeryUnits
---
+++ Frontend/src/pages/Furnitures/DiningRoom/DiningTables.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DiningTables = () => {
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  )
+}
+
+export default DiningTables
