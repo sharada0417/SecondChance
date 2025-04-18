92a09b7fa1bd4c6e521f3b44adc1598284bd4450
sharada417 <sharada.kanchana2001@gmail.com>
2025-04-18 12:27:01 +0530

    Update office room section

---
+++ Frontend/src/pages/Furnitures/OfficeRoom/Bookshelves.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bookshelves = () => {
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  )
+}
+
+export default Bookshelves
---
+++ Frontend/src/pages/Furnitures/OfficeRoom/Workstations.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Workstations = () => {
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  )
+}
+
+export default Workstations
