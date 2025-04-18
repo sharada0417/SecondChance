2de58a1225d19c6ed948f479e8f52dbc95c7e248
sharada417 <sharada.kanchana2001@gmail.com>
2025-04-18 12:11:20 +0530

    Update Kids section

--- Frontend/src/pages/Fashion/Kids/TShirts.jsx
+++ Frontend/src/pages/Fashion/Kids/TShirts.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Pants = () => {
+const TShirts = () => {
   return (
     <div>
       <h1>Hi</h1>
@@ -8,4 +8,4 @@ const Pants = () => {
   )
 }
 
-export default Pants
+export default TShirts
