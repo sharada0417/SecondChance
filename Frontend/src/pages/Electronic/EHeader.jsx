0342dc7e6ce43745567ad986a150e36afa828fb6
sharada417 <sharada.kanchana2001@gmail.com>
2025-04-19 23:04:47 +0530

    update Home electronics

--- Frontend/src/pages/Electronic/EHeader.jsx
+++ Frontend/src/pages/Electronic/EHeader.jsx
@@ -6,7 +6,7 @@ const EHeader = () => {
     {/* Header Section */}
     <div className="flex flex-col items-center bg-gradient-to-r from-green-400 via-green-500 to-green-600 p-8 shadow-2xl">
     <h3 className="text-4xl font-extrabold text-white mb-2 drop-shadow-md tracking-wide">
-        Home Electronics
+        Electronic
     </h3>
   </div>
   </>
