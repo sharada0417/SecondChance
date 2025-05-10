728fa61e81fcae25a5de31904a11035865df5e6b
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-10 19:03:36 +0530

    created OrderItemDto

---
+++ Backend/SecondChance/src/main/java/dto/OrderItemDto.java
@@ -0,0 +1,26 @@
+package dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItemDto {
+	private Long id;
+	private int quantity;
+	private BigDecimal price;
+	private String status;
+	private userDto user;
+	private ProductDto product;
+	private LocalDateTime createdAt;
+}
