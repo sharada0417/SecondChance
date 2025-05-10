cc1a277fd10fc27bc0adb826e9f1a3a69be73761
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-10 18:47:03 +0530

    fix error  addressDto

--- Backend/SecondChance/src/main/java/dto/AddressDto.java
+++ Backend/SecondChance/src/main/java/dto/AddressDto.java
@@ -1,10 +1,17 @@
 package dto;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +24,17 @@ import lombok.NoArgsConstructor;
 public class AddressDto {
 	
 	private Long id;
-	private String name;
-	private List <ProductDto> productList;
+	
+	private String street;
+	private String city;
+	private String state;
+	private String zipcode;
+	private String country;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="user_id")
+	private User user;
+	
+	@Column(name="created_at")
+	private final LocalDateTime createdAt = LocalDateTime.now();
 }
