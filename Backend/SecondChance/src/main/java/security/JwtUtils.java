bde9cf952442e4b2902d171b3ff01484cb3035a8
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-11 02:57:53 +0530

    create JwtAuthFilter security

--- Backend/SecondChance/src/main/java/security/JwtUtils.java
+++ Backend/SecondChance/src/main/java/security/JwtUtils.java
@@ -1,11 +1,20 @@
 package security;
 
-import javax.crypto.SecretKey;
-
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import lombok.extern.slf4j.Slf4j;
+import entity.User;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.function.Function;
 
 @Service
 @Slf4j
@@ -17,4 +26,40 @@ public class JwtUtils {
 	@Value("${secreteJwtString")
 	private String secreteJwString;
 	
+	  @PostConstruct
+	    private void init(){
+	        byte[] keyBytes = secreteJwString.getBytes(StandardCharsets.UTF_8);
+	        this.key = new SecretKeySpec(keyBytes, "HmacSHA256");
+	    }
+
+	    public String generateToken(User user){
+	        String username = user.getEmail();
+	        return generateToken(username);
+	    }
+
+	    public String generateToken(String username){
+	        return Jwts.builder()
+	                .subject(username)
+	                .issuedAt(new Date(System.currentTimeMillis()))
+	                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME_IN_MILLISEC))
+	                .signWith(key)
+	                .compact();
+	    }
+
+	    public String getUsernameFromToken(String token){
+	        return extractClaims(token, Claims::getSubject);
+	    }
+
+	    private <T> T extractClaims(String token, Function<Claims, T> claimsTFunction){
+	        return claimsTFunction.apply(Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload());
+	    }
+
+	    public boolean isTokenValid(String token, UserDetails userDetails){
+	        final String username = getUsernameFromToken(token);
+	        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	    }
+
+	    private boolean isTokenExpired(String token){
+	        return extractClaims(token, Claims::getExpiration).before(new Date());
+	    }
 }
