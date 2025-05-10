f8e6a4fa96ed005eb1d40c3d1383d4c81fc49710
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-10 21:35:08 +0530

    created OrderRequest dto

--- Backend/SecondChance/src/main/java/dto/OrderRequest.java
+++ Backend/SecondChance/src/main/java/dto/OrderRequest.java
@@ -1,5 +1,17 @@
 package dto;
 
-public class OrderRequest {
+import java.math.BigDecimal;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import entity.payment;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OrderRequest {
+	private BigDecimal totalPrice;
+	private List<OrderItemRequest> items;
+	private payment paymentInfo;
 }
