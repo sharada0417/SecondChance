b879248acd791fe185fda9cbea94f15adfd4b122
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-13 20:59:37 +0530

    create address controller

---
+++ Backend/SecondChance/src/main/java/controller/AddressController.java
@@ -0,0 +1,25 @@
+package controller;
+import lombok.RequiredArgsConstructor;
+import service.interf.AddressService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dto.AddressDto;
+import dto.Response;
+
+@RestController
+@RequestMapping("/address")
+@RequiredArgsConstructor
+public class AddressController {
+
+    private final AddressService addressService;
+
+    @PostMapping("/save")
+    public ResponseEntity<Response> saveAndUpdateAddress(@RequestBody AddressDto addressDto){
+        return ResponseEntity.ok(addressService.saveAndUpdateAddress(addressDto));
+    }
+}
